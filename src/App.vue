c68f14cbe7a407085f7afb142dcf05286b214594
Yusuke Miyazaki <ysk1180@gmail.com>
2019-04-27 21:20:01 +0900

    ベース作成・Bootstrap導入

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,59 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div>
+      <input type="radio" id="black" value="black" v-model="image">
+      <label for="black"><img src="./assets/black.png"></label>
+      <input type="radio" id="red" value="red" v-model="image">
+      <label for="red"><img src="./assets/red.png"></label>
+    </div>
+    <div>
+      <b-form-textarea v-model="msg" placeholder="ここに入力してください" rows="2"></b-form-textarea>
+    </div>
+    <div id="preview">
+      <img :src="image_path(image)" />
+      <p>{{ msg }}</p>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      msg: '',
+      image: 'black'
+    }
+  },
+  methods: {
+    image_path: function(value) {
+      return require("./assets/" + value + ".png")
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+#preview {
+  width: 315px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: inline-block;
+  margin-bottom: 20px;
+  position: relative;
+}
+#preview p {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -ms-transform: translate(-50%,-50%);
+  -webkit-transform: translate(-50%,-50%);
+  transform: translate(-50%,-50%);
+  margin:0;
+  padding:0;
+  font-size: 35px;
+  color: white;
+  white-space: pre;
+  line-height: 1em;
+  text-align: initial;
+  font-weight: bold;
 }
 </style>
